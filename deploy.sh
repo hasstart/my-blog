171649e500317b2a5312d21ee9a98a00d4b08f44
zhouyalong <15515533605@163.com>
2022-02-22 16:40:25 +0800

    chore: 设置git

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,9 @@ npm run build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
+git config --global user.name "zhouyalong"
+git config --global user.email "15515533605@163.com"
+
 # deploy to github
 if [ -z "$ACCESS_TOKEN" ]; then
   msg='deploy'
@@ -16,8 +19,7 @@ if [ -z "$ACCESS_TOKEN" ]; then
 else
   msg='来自 github actions的自动部署'
   githubUrl=https://zhouyalongs:${ACCESS_TOKEN}@github.com/zhouyalongs/Bloging.git
-  git config --global user.name "zhouyalong"
-  git config --global user.email "15515533605@163.com"
+
 fi
 git init
 git add -A
