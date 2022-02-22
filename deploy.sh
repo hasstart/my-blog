bf26a18927eea9fff9b124fda890c8b7bcb8bf89
zhouyalong <zhouyalong@meishubao.com>
2022-02-22 15:54:19 +0800

    chore: ci文件调整

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,20 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github
-
+if [ -z "$ACCESS_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:zhouyalongs/Bloging.git
+else
+  msg='来自 github actions的自动部署'
+  githubUrl=https://zhouyalongs:${ACCESS_TOKEN}@github.com/zhouyalongs/Bloging.git
+  git config --global user.name "zhouyalong"
+  git config --global user.email "15515533605@163.com"
+fi
 git init
 git add -A
-git commit -m "来自github actions的自动部署"
-git push -f git@github.com:zhouyalongs/my-blog.git master:blog-pages # 推送到github
+git commit -m "${msg}"
+git push -f $githubUrl master:blogPages # 推送到github
+
 
 cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
